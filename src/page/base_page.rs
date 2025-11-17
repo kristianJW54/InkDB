75c4d164ae68967876e4f13e505ebfb64ef86877
kristianJW54 <kristian15994@gmail.com>
2025-11-17 21:56:47 +0000

    constructing page wiring and structures

--- src/page/base_page.rs
+++ src/page/base_page.rs
@@ -1,14 +1,62 @@
+use std::cell::UnsafeCell;
+use std::ops::Deref;
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
+pub(crate) type RawPage = [u8; 4096];
 
-
-pub(crate) struct RawPage{
-    data: [u8; 4096],
+// We can use this when we need more than closures, for more complex operations
+struct InnerRawReadGuard<'a> {
+    latch_read: RwLockReadGuard<'a, ()>,
+    data: &'a RawPage,
 }
 
-impl RawPage {
-    pub fn write(&mut self, bytes: &mut [u8]) -> Result<(),()> {
-        self.data.copy_from_slice(bytes);
-        Ok(())
+impl <'a> Deref for InnerRawReadGuard<'a> {
+    type Target = RawPage;
+    fn deref(&self) -> &Self::Target {
+        self.data
+    }
+}
+
+// InnerPage is a simple container that holds raw bytes and a latch. It does not know about page types
+// or implementations concerned with the raw bytes
+pub(crate) struct InnerPage {
+    latch: RwLock<()>,
+    raw_page: UnsafeCell<RawPage>,
+}
+
+impl InnerPage {
+    pub fn new() -> Self {
+        Self { latch: RwLock::new(()), raw_page: UnsafeCell::new([0u8; 4096]) }
+    }
+
+    fn read_guard<'a>(&self) -> InnerRawReadGuard<'_> {
+        let guard = self.latch.read().unwrap();
+        InnerRawReadGuard{ latch_read: guard, data: unsafe { &*self.raw_page.get() } }
+    }
+
+    // pub fn read<P>(&self, f: FnOnce(&RawPage) -> P) -> P {
+    //     let guard = self.read_guard();
+    //     f(guard.data)
+    // }
+
+    pub fn print_data(&self) {
+        let read_guard = self.read_guard();
+        println!("{:?}", read_guard.data);
+    }
+
+}
+
+pub(crate) struct HeapPageRef<'inner> {
+    raw: InnerRawReadGuard<'inner>,
+}
+
+impl<'inner> HeapPageRef<'inner> {
+    pub fn new(page: &'inner InnerPage) -> Self {
+        Self { raw: page.read_guard() }
+    }
+
+    pub fn fake_id(&self) -> [u8; 2] {
+        self.raw[..2].try_into().unwrap()
     }
 }
 
--- src/page/page.rs
+++ src/page/page.rs
@@ -6,14 +6,14 @@ use std::cell::{UnsafeCell};
 use std::marker::PhantomData;
 use std::sync::atomic::AtomicBool;
 use std::sync::RwLock;
-use crate::page::base_page::RawPage;
+use crate::page::base_page::{InnerPage, RawPage};
 use crate::page::{PageID, PageType};
 
 // The Page will be shared and referenced by many different things (transaction, cache, reader, writer)
 pub(crate) struct Page {
     id: PageID,
     // We need to be able to get a mutable view on page despite having many shared references active
-    page_inner: UnsafeCell<RawPage>,
+    page_inner: InnerPage,
     // We store the latch separate to quickly capture what we need and then perform operations outside the latch
     // which we know don't conflict due to MVCC
     latch: RwLock<()>,
@@ -22,18 +22,29 @@ pub(crate) struct Page {
 
 impl Page {
     pub fn new(id: PageID, bytes: RawPage) -> Self {
-        Self { id, page_inner: UnsafeCell::new(bytes), latch: RwLock::new(()), dirty: AtomicBool::new(false) }
+        Self { id, page_inner: InnerPage::new(), latch: RwLock::new(()), dirty: AtomicBool::new(false) }
     }
 
-    pub fn write(&mut self, bytes: &mut [u8]) -> Result<(), ()> {
+    pub fn write(&self, bytes: &mut [u8]) -> Result<(), ()> {
 
         assert!(bytes.len() <= 1000);
 
-        // We need to lock and then we can get_mut page and then write
-
-        let guard = self.latch.write().unwrap();
-        let page_data = self.page_inner.get_mut();
-        page_data.write(bytes).map_err(|_| ())?;
         Ok(())
     }
+
+    pub fn print_data(&self) {
+        self.page_inner.print_data();
+    }
+}
+
+
+// TODO Test here
+
+#[test]
+fn test_page_print() {
+
+    let page = Page::new(PageID(2), RawPage::new());
+
+
+
 }
