d1a6fd579f0e9592318701e1b55ac36cd99f63ab
kristianJW54 <kristian15994@gmail.com>
2025-11-18 19:39:00 +0000

    happy with the basic layers of the page + now need to build out and test hard

--- src/page/base_page.rs
+++ src/page/base_page.rs
@@ -1,37 +1,55 @@
 use std::cell::UnsafeCell;
-use std::ops::Deref;
-use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::ops::{Deref, DerefMut};
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 pub(crate) type RawPage = [u8; 4096];
 
 // We can use this when we need more than closures, for more complex operations
-struct InnerRawReadGuard<'a> {
-    latch_read: RwLockReadGuard<'a, ()>,
-    data: &'a RawPage,
+#[derive(Debug)]
+struct PageReadGuard<'a> {
+    latch_read: RwLockReadGuard<'a, RawPage>,
 }
 
-impl <'a> Deref for InnerRawReadGuard<'a> {
+impl <'a> Deref for PageReadGuard<'a> {
     type Target = RawPage;
     fn deref(&self) -> &Self::Target {
-        self.data
+        &*self.latch_read
     }
+}
+
+struct PageWriteGuard<'a> {
+    latch_write: RwLockWriteGuard<'a, RawPage>
+}
+
+impl <'a> Deref for PageWriteGuard<'a> {
+    type Target = RawPage;
+    fn deref(&self) -> &Self::Target { &*self.latch_write }
+}
+
+impl <'a> DerefMut for PageWriteGuard<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target { &mut *self.latch_write }
 }
 
 // InnerPage is a simple container that holds raw bytes and a latch. It does not know about page types
 // or implementations concerned with the raw bytes
 pub(crate) struct InnerPage {
-    latch: RwLock<()>,
-    raw_page: UnsafeCell<RawPage>,
+    page: RwLock<RawPage>,
 }
 
 impl InnerPage {
+
     pub fn new() -> Self {
-        Self { latch: RwLock::new(()), raw_page: UnsafeCell::new([0u8; 4096]) }
+        InnerPage { page: RwLock::new([0; 4096]) }
     }
 
-    fn read_guard<'a>(&self) -> InnerRawReadGuard<'_> {
-        let guard = self.latch.read().unwrap();
-        InnerRawReadGuard{ latch_read: guard, data: unsafe { &*self.raw_page.get() } }
+    fn read_guard(&self) -> PageReadGuard<'_> {
+        let guard = self.page.read().unwrap();
+        PageReadGuard { latch_read: guard }
+    }
+
+    fn write_guard(&self) -> PageWriteGuard<'_> {
+        let guard = self.page.write().unwrap();
+        PageWriteGuard { latch_write: guard }
     }
 
     // pub fn read<P>(&self, f: FnOnce(&RawPage) -> P) -> P {
@@ -41,24 +59,77 @@ impl InnerPage {
 
     pub fn print_data(&self) {
         let read_guard = self.read_guard();
-        println!("{:?}", read_guard.data);
+        println!("{:?}", read_guard);
     }
 
 }
 
-pub(crate) struct HeapPageRef<'inner> {
-    raw: InnerRawReadGuard<'inner>,
+//------------------------- Page specific types ------------------------------//
+
+struct HeapPageRead<'page> {
+    data: PageReadGuard<'page>,
 }
 
-impl<'inner> HeapPageRef<'inner> {
-    pub fn new(page: &'inner InnerPage) -> Self {
-        Self { raw: page.read_guard() }
+impl <'page> HeapPageRead<'page> {
+
+    fn from_guard(guard: PageReadGuard<'page>) -> Self {
+        Self { data: guard }
     }
 
-    pub fn fake_id(&self) -> [u8; 2] {
-        self.raw[..2].try_into().unwrap()
+    fn get_first_byte(&self) -> usize {
+        self.data[0] as usize
+    }
+
+}
+
+struct HeapPageMut<'page> {
+    data: PageWriteGuard<'page>,
+}
+
+impl <'page> HeapPageMut<'page> {
+    fn from_guard(guard: PageWriteGuard<'page>) -> Self {
+        Self { data: guard }
+    }
+
+    fn write_first_byte(&mut self, byte: u8) {
+        self.data[0] = byte;
     }
 }
+
+#[test]
+fn two_threads() {
+
+    let page = Arc::new(InnerPage::new());
+
+    let mut threads = Vec::new();
+
+    let p1 = page.clone();
+    let thread1 = std::thread::spawn(move || {
+        let b = HeapPageRead::from_guard(p1.read_guard()).get_first_byte();
+        println!("first byte: {}", b);
+    });
+
+    threads.push(thread1);
+
+    let p2 = page.clone();
+    let thread2 = std::thread::spawn(move || {
+
+        let w = HeapPageMut::from_guard(p2.write_guard()).write_first_byte(2);
+        println!("first byte: {}", HeapPageRead::from_guard(p2.read_guard()).get_first_byte());
+
+        let b = HeapPageRead::from_guard(p2.read_guard()).get_first_byte();
+        println!("first byte: {}", b);
+    });
+
+    threads.push(thread2);
+
+    for t in threads {
+        t.join().unwrap();
+    }
+
+}
+
+
 
 // We need two page types initially, Heap and Index.
 // We will need a header, slot array and cell
--- src/page/page.rs
+++ src/page/page.rs
@@ -1,50 +1,25 @@
-
-
-// The main page frame which is stored in the page_cache
-
-use std::cell::{UnsafeCell};
-use std::marker::PhantomData;
 use std::sync::atomic::AtomicBool;
-use std::sync::RwLock;
-use crate::page::base_page::{InnerPage, RawPage};
-use crate::page::{PageID, PageType};
+use crate::page::base_page::InnerPage;
+use crate::page::PageID;
 
-// The Page will be shared and referenced by many different things (transaction, cache, reader, writer)
-pub(crate) struct Page {
+pub (crate) struct PageFrame {
     id: PageID,
-    // We need to be able to get a mutable view on page despite having many shared references active
-    page_inner: InnerPage,
-    // We store the latch separate to quickly capture what we need and then perform operations outside the latch
-    // which we know don't conflict due to MVCC
-    latch: RwLock<()>,
+    //txid?
     dirty: AtomicBool,
+    inner_page: InnerPage,
+    // more meta data
 }
 
-impl Page {
-    pub fn new(id: PageID, bytes: RawPage) -> Self {
-        Self { id, page_inner: InnerPage::new(), latch: RwLock::new(()), dirty: AtomicBool::new(false) }
+impl PageFrame {
+    
+    pub fn new() -> Self {
+        Self { id: PageID(1), dirty: AtomicBool::new(false), inner_page: InnerPage::new() }
     }
-
-    pub fn write(&self, bytes: &mut [u8]) -> Result<(), ()> {
-
-        assert!(bytes.len() <= 1000);
-
-        Ok(())
-    }
-
-    pub fn print_data(&self) {
-        self.page_inner.print_data();
-    }
+    
 }
 
-
-// TODO Test here
 
 #[test]
-fn test_page_print() {
-
-    let page = Page::new(PageID(2), RawPage::new());
-
-
-
+fn print_page() {
+    let page = PageFrame::new().inner_page.print_data();
 }
